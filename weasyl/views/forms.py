aac08840cf4d355462739cec4e4518fe4bda5529
weykent <weykent@weasyl.com>
2014-02-16 00:43:41 -0800

    Add form error display.

--- weasyl/views/forms.py
+++ weasyl/views/forms.py
@@ -2,19 +2,21 @@ import logging
 
 import colander as c
 from pyramid_deform import FormView as _FormView
+from translationstring import TranslationStringFactory
 
 from ..legacy import login_name
 from ..models.users import Login
 
 
+_ = TranslationStringFactory(__name__)
 log = logging.getLogger(__name__)
 
 
 class FormView(_FormView):
     def failure(self, exc):
-        log.debug('form failed to validate: %r',
-                  [(b.node, list(c.interpolate(b.messages()))) for a, b in exc.error.paths()])
-        return {'form': exc.field}
+        errors = [(t[-1].node, list(c.interpolate(t[-1].messages()))) for t in exc.error.paths()]
+        log.debug('form failed to validate: %r', errors)
+        return {'form': exc.field, 'errors': errors}
 
     def show(self, form):
         return {'form': form}
@@ -31,5 +33,5 @@ class User(c.SchemaType):
             return cstruct
         user = Login.query.filter_by(login_name=login_name(cstruct)).first()
         if user is None:
-            raise c.Invalid(node, '%r is not a valid username' % (cstruct,))
+            raise c.Invalid(node, _('"${val}" is not a valid username', mapping=dict(val=cstruct)))
         return user
