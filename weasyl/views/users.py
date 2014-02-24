8c810bf0f0ef5ac977edc8c6facec2f47601312b
weykent <weykent@weasyl.com>
2014-02-23 19:56:05 -0800

    Refactor templates; add user works.

--- weasyl/views/users.py
+++ weasyl/views/users.py
@@ -37,4 +37,24 @@ def view_user(context, request):
         'user': context.user,
         'submissions': submissions,
         'featured': featured,
+        'sidebar': None,
+    }
+
+
+@view_config(name='works', context=UserResource, renderer='users/works.jinja2', api='false', permission='view')
+@view_config(name='works', context=UserResource, renderer='json', api='true', permission='view')
+def view_user_works(context, request):
+    submissions = (
+        Submission.query
+        .filter(Submission.userid == context.user.userid)
+        .order_by(Submission.unixtime.desc())
+        .limit(30)
+        .all())
+
+    media.populate_with_submission_media(submissions)
+
+    return {
+        'user': context.user,
+        'submissions': submissions,
+        'sidebar': 'works',
     }
