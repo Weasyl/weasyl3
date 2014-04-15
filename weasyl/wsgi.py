a8972c77cfd339d2698ec244339d004c14133c76
Charmander <campersander@gmail.com>
2014-04-15 10:04:23 -0700

    > ugly hack to make None work as a root

--- weasyl/wsgi.py
+++ weasyl/wsgi.py
@@ -7,6 +7,7 @@ from pyramid.config import Configurator
 from pyramid.renderers import JSON
 from pyramid.traversal import quote_path_segment
 from pyramid import url
+from pyramid import traversal
 
 from .models.meta import configure as configure_db
 from .resources import RootResource
@@ -38,6 +39,56 @@ def _join_elements(elements):
 
 # ugly hack to make ~ not escaped in URLs
 url._join_elements = _join_elements
+
+
+class _Root:
+    pass
+
+_Root.__name__ = ''
+
+
+def _lineage(resource):
+    """
+    Return a generator representing the :term:`lineage` of the
+    :term:`resource` object implied by the ``resource`` argument.  The
+    generator first returns ``resource`` unconditionally.  Then, if
+    ``resource`` supplies a ``__parent__`` attribute, return the resource
+    represented by ``resource.__parent__``.  If *that* resource has a
+    ``__parent__`` attribute, return that resource's parent, and so on,
+    until the resource being inspected either has no ``__parent__``
+    attribute or which has a ``__parent__`` attribute of ``None``.
+    For example, if the resource tree is::
+
+      thing1 = Thing()
+      thing2 = Thing()
+      thing2.__parent__ = thing1
+
+    Calling ``lineage(thing2)`` will return a generator.  When we turn
+    it into a list, we will get::
+
+      list(lineage(thing2))
+      [ <Thing object at thing2>, <Thing object at thing1> ]
+    """
+    if resource is None:
+        yield _Root
+        return
+
+    while resource is not None:
+        yield resource
+        # The common case is that the AttributeError exception below
+        # is exceptional as long as the developer is a "good citizen"
+        # who has a root object with a __parent__ of None.  Using an
+        # exception here instead of a getattr with a default is an
+        # important micro-optimization, because this function is
+        # called in any non-trivial application over and over again to
+        # generate URLs and paths.
+        try:
+            resource = resource.__parent__
+        except AttributeError:
+            resource = None
+
+# ugly hack to make None work as a root
+traversal.lineage = _lineage
 
 
 def make_app(global_config, **settings):
