673b042658e0f4600f56c01f0482eca0d6fd6dbd
weykent <weykent@weasyl.com>
2014-08-27 06:57:39 -0400

    Don't log WeasylErrors to sentry.
    A WeasylError should be an 'expected' error; we only care about unexpected
    errors being logged.

--- weasyl/views/exceptions.py
+++ weasyl/views/exceptions.py
@@ -2,6 +2,7 @@ import logging
 
 from pyramid.view import view_config
 
+from ..exceptions import WeasylError
 from ..sessions import make_session_id
 
 
@@ -11,7 +12,7 @@ log = logging.getLogger(__name__)
 @view_config(context=Exception, renderer='errors/generic.jinja2', api='false')
 @view_config(context=Exception, renderer='json', api='true')
 def exception_catchall(exc, request):
-    if 'sentry.log_error' in request.environ:
+    if not isinstance(exc, WeasylError) and 'sentry.log_error' in request.environ:
         request_id = make_session_id(8)
         exc_info = type(exc), exc, exc.__traceback__
         event_id, = request.environ['sentry.log_error'](exc_info, request_id=request_id)
