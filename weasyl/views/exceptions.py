cde6c3e826e1c450c01ac132b2a8b783296fe8c7
weykent <weykent@weasyl.com>
2014-08-27 05:42:43 -0400

    Add a view for catching exceptions.

---
+++ weasyl/views/exceptions.py
@@ -0,0 +1,25 @@
+import logging
+
+from pyramid.view import view_config
+
+from ..sessions import make_session_id
+
+
+log = logging.getLogger(__name__)
+
+
+@view_config(context=Exception, renderer='errors/generic.jinja2', api='false')
+@view_config(context=Exception, renderer='json', api='true')
+def exception_catchall(exc, request):
+    if 'sentry.log_error' in request.environ:
+        request_id = make_session_id(8)
+        exc_info = type(exc), exc, exc.__traceback__
+        event_id, = request.environ['sentry.log_error'](exc_info, request_id=request_id)
+    else:
+        request_id = event_id = None
+    return {
+        'error': True,
+        'event_id': event_id,
+        'request_id': request_id,
+        'message': str(exc),
+    }
