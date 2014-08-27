9aac82d87354c27eca1bcbb7a049811e13424901
weykent <weykent@weasyl.com>
2014-08-27 14:44:45 -0400

    Remove some unused code.

--- weasyl/views/decorators.py
+++ weasyl/views/decorators.py
@@ -5,14 +5,6 @@ from pyramid.renderers import render_to_response
 from pyramid.view import view_config
 
 
-@decorator
-def requires_no_login(f, request):
-    if request.session['userid']:
-        return render_to_response(
-            'errors/generic.jinja2', {'message': 'You must be signed out.'})
-    return f(request)
-
-
 def also_api_view(*, template, **view_args):
     def deco(func):
         @view_config(_depth=1, renderer=template, api='false', **view_args)
