76fc9919f79513e59f90da3e62b231eb8e2fabfa
weykent <weykent@weasyl.com>
2014-08-27 16:53:50 -0400

    Add /api/v2/version.

--- weasyl/views/api.py
+++ weasyl/views/api.py
@@ -3,6 +3,7 @@ import logging
 from pyramid.view import view_config
 
 from ..resources import APIv2Resource
+from .. import _version
 
 
 log = logging.getLogger(__name__)
@@ -20,3 +21,17 @@ def whoami(request):
             'login': None,
             'userid': 0,
         }
+
+
+@view_config(name='version', context=APIv2Resource, renderer='json', api='true')
+@view_config(name='version.json', context=APIv2Resource, renderer='json', api='true')
+def json_version(request):
+    return {
+        'short_sha': _version.__revision__.lstrip('g'),
+        'version': _version.__version__,
+    }
+
+
+@view_config(name='version.txt', context=APIv2Resource, renderer='string', api='true')
+def text_version(request):
+    return '{0.__version__}-{0.__revision__}'.format(_version)
