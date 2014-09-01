ab6d876cc39632228a9568c5ed2a83037620ff31
weykent <weykent@weasyl.com>
2014-09-01 12:19:16 -0400

    Use exceptions' error codes; be more terse for unexpected errors.

--- weasyl/views/exceptions.py
+++ weasyl/views/exceptions.py
@@ -11,7 +11,7 @@ log = logging.getLogger(__name__)
 @also_api_view(context=Exception, template='errors/generic.jinja2')
 def exception_catchall(exc, request):
     expected = isinstance(exc, ExpectedWeasylError)
-    request.response.status = 200 if expected else 500
+    request.response.status = type(exc).code if expected else 500
     if not expected and 'sentry.log_error' in request.environ:
         request_id = make_session_id(8)
         event_id, = request.environ['sentry.log_error'](request.exc_info, request_id=request_id)
@@ -21,11 +21,15 @@ def exception_catchall(exc, request):
                 'an error occurred, but sentry was not configured to capture it',
                 exc_info=request.exc_info)
         request_id = event_id = None
-    return {
+    ret = {
         'error': True,
         'event_id': event_id,
         'request_id': request_id,
-        'message': str(exc),
-        'code': type(exc).__name__,
-        'description': type(exc).__doc__,
     }
+    if expected:
+        ret.update({
+            'message': str(exc),
+            'code': type(exc).__name__,
+            'description': type(exc).__doc__,
+        })
+    return ret
