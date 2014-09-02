f1e44fbe9c0753f068075dccad25e54001048b58
weykent <weykent@weasyl.com>
2014-09-02 14:09:12 -0400

    M-x untabify

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,56 +1,56 @@
 module.exports = function (grunt) {
     grunt.initConfig({
-	uglify: {
-	    js: {
-		files: {
-		    'assets/js/site.js': [
-			'assets/js/scripts.js',
-		    ],
-		},
-	    },
-	},
+        uglify: {
+            js: {
+                files: {
+                    'assets/js/site.js': [
+                        'assets/js/scripts.js',
+                    ],
+                },
+            },
+        },
 
-	sass: {
-	    site: {
-		options: {
-		    style: 'compressed',
-		},
-		files: {
-		    'assets/css/site.css': [
-			'assets/sass/public.sass',
-		    ],
-		}
-	    },
-	},
+        sass: {
+            site: {
+                options: {
+                    style: 'compressed',
+                },
+                files: {
+                    'assets/css/site.css': [
+                        'assets/sass/public.sass',
+                    ],
+                }
+            },
+        },
 
-	filerev: {
-	    options: {
-		encoding: 'utf8',
-		algorithm: 'sha256',
-		length: 8,
-	    },
-	    images: {
-		src: 'assets/images/*.{jpg,png,svg}',
-		dest: 'weasyl/static/images',
-	    },
-	    js: {
-		src: 'assets/js/site.js',
-		dest: 'weasyl/static',
-	    },
-	    css: {
-		src: 'assets/css/site.css',
-		dest: 'weasyl/static',
-	    },
-	},
+        filerev: {
+            options: {
+                encoding: 'utf8',
+                algorithm: 'sha256',
+                length: 8,
+            },
+            images: {
+                src: 'assets/images/*.{jpg,png,svg}',
+                dest: 'weasyl/static/images',
+            },
+            js: {
+                src: 'assets/js/site.js',
+                dest: 'weasyl/static',
+            },
+            css: {
+                src: 'assets/css/site.css',
+                dest: 'weasyl/static',
+            },
+        },
 
-	filerev_assets: {
-	    dist: {
-		options: {
-		    dest: 'weasyl/assets.json',
-		    cwd: 'weasyl/',
-		},
-	    },
-	},
+        filerev_assets: {
+            dist: {
+                options: {
+                    dest: 'weasyl/assets.json',
+                    cwd: 'weasyl/',
+                },
+            },
+        },
     });
 
     grunt.loadNpmTasks('grunt-filerev');
@@ -59,5 +59,5 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-sass');
 
     grunt.registerTask(
-	'default', ['uglify', 'sass', 'filerev', 'filerev_assets']);
+        'default', ['uglify', 'sass', 'filerev', 'filerev_assets']);
 };
