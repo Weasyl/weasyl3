cbab1d6681efac1661113f5047331b8a11cdd030
weykent <weykent@weasyl.com>
2014-10-05 22:18:21 -0700

    Factor out ugly hacks into their own module.

--- weasyl/wsgi.py
+++ weasyl/wsgi.py
@@ -1,5 +1,4 @@
 import datetime
-import functools
 import json
 
 import arrow
@@ -9,8 +8,7 @@ from pyramid.authentication import SessionAuthenticationPolicy
 from pyramid.config import Configurator
 from pyramid.renderers import JSON
 from pyramid.security import authenticated_userid
-from pyramid.traversal import quote_path_segment
-from pyramid import httpexceptions, traversal, url
+from pyramid import httpexceptions, url
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy import engine_from_config
 from zope.sqlalchemy import ZopeTransactionExtension
@@ -22,7 +20,7 @@ from .resources import RootResource
 from .sessions import WeasylSession
 from .views.legacy import configure_urls
 from .views.login import login_forms
-from . import authentication, authorization, predicates
+from . import authentication, authorization, hacks, predicates
 
 
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
@@ -70,64 +68,6 @@ def db(request):
     return DBSession()
 
 
-@functools.lru_cache(1000)
-def _join_elements(elements):
-    return '/'.join([quote_path_segment(s, safe=':@&+$,~') for s in elements])
-
-# ugly hack to make ~ not escaped in URLs
-url._join_elements = _join_elements
-
-
-class _Root:
-    pass
-
-_Root.__name__ = ''
-
-
-def _lineage(resource):
-    """
-    Return a generator representing the :term:`lineage` of the
-    :term:`resource` object implied by the ``resource`` argument.  The
-    generator first returns ``resource`` unconditionally.  Then, if
-    ``resource`` supplies a ``__parent__`` attribute, return the resource
-    represented by ``resource.__parent__``.  If *that* resource has a
-    ``__parent__`` attribute, return that resource's parent, and so on,
-    until the resource being inspected either has no ``__parent__``
-    attribute or which has a ``__parent__`` attribute of :py:data:`None`.
-    For example, if the resource tree is::
-
-      thing1 = Thing()
-      thing2 = Thing()
-      thing2.__parent__ = thing1
-
-    Calling ``lineage(thing2)`` will return a generator.  When we turn
-    it into a list, we will get::
-
-      list(lineage(thing2))
-      [ <Thing object at thing2>, <Thing object at thing1> ]
-    """
-    if resource is None:
-        yield _Root
-        return
-
-    while resource is not None:
-        yield resource
-        # The common case is that the AttributeError exception below
-        # is exceptional as long as the developer is a "good citizen"
-        # who has a root object with a __parent__ of None.  Using an
-        # exception here instead of a getattr with a default is an
-        # important micro-optimization, because this function is
-        # called in any non-trivial application over and over again to
-        # generate URLs and paths.
-        try:
-            resource = resource.__parent__
-        except AttributeError:
-            resource = None
-
-# ugly hack to make None work as a root
-traversal.lineage = _lineage
-
-
 class NoLeadingUnderscoresJSON(JSON):
     def __call__(self, info):
         super_render = super().__call__(info)
@@ -158,6 +98,8 @@ def make_asset_path_finder():
 
 
 def make_app(global_config, **settings):
+    hacks.install()
+
     settings['deform_jinja2.template_search_path'] = 'weasyl:widgets'
     settings['jinja2.filters'] = """
         asset_path = weasyl.filters.asset_path_filter
