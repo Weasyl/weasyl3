b0c64dae0a1b65dc42f2051d67307051653f2f68
tdixon <taeganong@gmail.com>
2015-10-24 22:20:34 -0300

    Add short SHA of current build to page footer

---
+++ weasyl/views/global.py
@@ -0,0 +1,17 @@
+import subprocess
+
+from pyramid.events import subscriber
+from pyramid.events import BeforeRender
+
+CURRENT_SHA = subprocess.check_output(["git", "rev-parse", "--short", "HEAD"]).decode("utf-8").strip()
+
+
+@subscriber(BeforeRender)
+def add_global(event):
+    """
+        Hooks the before-render event to inject
+        application-global variables into all
+        templates before they are rendered
+    """	
+    event['SHA'] = CURRENT_SHA
+
